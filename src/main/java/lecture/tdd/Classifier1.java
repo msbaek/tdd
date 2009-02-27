1f6d4ad0c14a9b750f56926642593e13d6765ef5
Myeongseok Baek <msbaek@daumcorp.com>
2009-02-27 13:02:41 +0900

    Classifier1Test#isFactor, factorsFor 추가

--- src/main/java/lecture/tdd/Classifier1.java
+++ src/main/java/lecture/tdd/Classifier1.java
@@ -4,4 +4,8 @@ public class Classifier1 {
 	public static boolean isFactor(int factor, int number) {
 		return number % factor == 0;
 	}
+
+	public static int[] factorsFor(int number) {
+		return new int[] { number };
+	}
 }
--- src/test/java/lecture/tdd/Classifier1Test.java
+++ src/test/java/lecture/tdd/Classifier1Test.java
@@ -1,11 +1,19 @@
 package lecture.tdd;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.*;
+import static org.junit.Assert.*;
+
 public class Classifier1Test {
-	@Test public void is_factor() {
+	@Test
+	public void is_factor() {
 		assertTrue(Classifier1.isFactor(1, 10));
+	}
+
+	@Test
+	public void factors_for() {
+		int[] expected = new int[] { 1 };
+		assertThat(Classifier1.factorsFor(1), is(expected));
 	}
 }
