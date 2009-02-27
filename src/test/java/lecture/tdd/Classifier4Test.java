14c6e525a214ea12965869096b74e9fec88c7d2b
Myeongseok Baek <msbaek@daumcorp.com>
2009-02-27 15:13:22 +0900

    more tests

--- src/test/java/lecture/tdd/Classifier4Test.java
+++ src/test/java/lecture/tdd/Classifier4Test.java
@@ -11,14 +11,27 @@ import static org.hamcrest.core.Is.*;
 
 public class Classifier4Test {
 	@Test
-	public void factors_for_6() {
-		Set<Integer> expected = expectionSetWith(1, 2, 3, 6);
-	    Classifier4 c = new Classifier4(6);
-	    c.calculateFactors();
-	    assertThat(c.getFactors(), is(expected));
+	public void factors_for_100() {
+		Classifier4 c = new Classifier4(100);
+		c.calculateFactors();
+		assertThat(c.getFactors(), is(expectationSetWith(1, 100, 2, 50, 4, 25, 5, 20, 10)));
 	}
 
-	private HashSet<Integer> expectionSetWith(Integer ... numbers) {
+	@Test
+	public void factors_for_12() {
+		Classifier4 c = new Classifier4(12);
+		c.calculateFactors();
+		assertThat(c.getFactors(), is(expectationSetWith(1, 2, 3, 4, 6, 12)));
+	}
+
+	@Test
+	public void factors_for_6() {
+		Classifier4 c = new Classifier4(6);
+		c.calculateFactors();
+		assertThat(c.getFactors(), is(expectationSetWith(1, 2, 3, 6)));
+	}
+
+	private Set<Integer> expectationSetWith(Integer... numbers) {
 		return new HashSet<Integer>(Arrays.asList(numbers));
 	}
 }
