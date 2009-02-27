12d3ab103ea7410ba89c3c128b4f1b49304b0fd5
Myeongseok Baek <msbaek@daumcorp.com>
2009-02-27 12:26:15 +0900

    3. failing test를 success할 만큼 코드 수정

--- src/main/java/lecture/tdd/Classifier1.java
+++ src/main/java/lecture/tdd/Classifier1.java
@@ -1,7 +1,7 @@
 package lecture.tdd;
 
 public class Classifier1 {
-	public static boolean isFactor(int number, int factor) {
-		throw new IllegalAccessError("Classifier1#isFactor is not implemented yet ^^^");
+	public static boolean isFactor(int factor, int number) {
+		return number % factor == 0;
 	}
 }
